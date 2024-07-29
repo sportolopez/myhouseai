a4c9d714252e58399baf4454de3b95b9f7333330
Sporto <sporto@certant.com>
2024-07-29 14:23:53 -0300

    avance

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -68,12 +68,12 @@ class DefaultController extends AbstractController
         $usuario = $usuarioRepository->findOneByEmail($jwtPayload->token_info->email);
 
         if($usuario->getCantidadImagenesDisponibles()<1)
-            return new JsonResponse(['error' => 'Te quedaste sin imagenes bb']);
+            return new JsonResponse(['error' => 'Te quedaste sin imagenes'],Response::HTTP_FORBIDDEN);
         // Obtener los datos de la solicitud
         $data = json_decode($request->getContent(), true);
 
         if (!isset($data['image']) && !isset($data['generation_id'])) {
-            return new JsonResponse(        ['error' => 'Se tiene que subir una imagen o un generation_id'],
+            return new JsonResponse(['error' => 'Se tiene que subir una imagen o un generation_id'],
             Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
