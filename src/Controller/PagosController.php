30c90738dc9b16cd1c2c60348b63f971fbfdc49a
Sporto <sporto@certant.com>
2024-08-20 12:08:28 -0300

    pagos

--- src/Controller/PagosController.php
+++ src/Controller/PagosController.php
@@ -5,8 +5,11 @@ use App\Entity\Planes;
 use App\Repository\PlanesRepository;
 use App\Repository\UsuarioRepository;
 use MercadoPago\Client\Common\RequestOptions;
+use MercadoPago\Client\Preference\PreferenceClient;
 use MercadoPago\Exceptions\MPApiException;
 use Doctrine\Persistence\ManagerRegistry;
+use MercadoPago\Resources\MerchantOrder\Item;
+use MercadoPago\Resources\Preference;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use MercadoPago\Client\Payment\PaymentClient;
 use MercadoPago\MercadoPagoConfig;
@@ -15,7 +18,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class PagosController extends AbstractController{
+
+    protected function authenticate()
+    {
+        // Getting the access token from .env file (create your own function)
+        // $mpAccessToken = getVariableFromEnv('mercado_pago_access_token');
+        // Set the token the SDK's config
+        MercadoPagoConfig::setAccessToken("TEST-4972941314108448-060716-d04a68936cfd0dea91f921cba2cdb2ee-22465532");
+        // (Optional) Set the runtime enviroment to LOCAL if you want to test on localhost
+        // Default value is set to SERVER
+        MercadoPagoConfig::setRuntimeEnviroment(MercadoPagoConfig::LOCAL);
+    }
+
+
 
     #[Route('/process_payment', name: 'create_payment', methods: ['POST'])]
     public function processPayment(ManagerRegistry $doctrine,Request $request, UsuarioRepository $usuarioRepository, PlanesRepository  $planesRepository): JsonResponse
@@ -27,11 +44,7 @@ class PagosController extends AbstractController{
         // Obtener datos de la solicitud
         $data = $request->toArray();
     
-        // Configurar MercadoPago
-        MercadoPagoConfig::setAccessToken("TEST-4972941314108448-060716-d04a68936cfd0dea91f921cba2cdb2ee-22465532");
-        // Step 2.1 (optional - default is SERVER): Set your runtime enviroment from MercadoPagoConfig::RUNTIME_ENVIROMENTS
-        // In case you want to test in your local machine first, set runtime enviroment to LOCAL
-        MercadoPagoConfig::setRuntimeEnviroment(MercadoPagoConfig::LOCAL);
+        self::authenticate();
         // Inicializar el cliente de API
         $client = new PaymentClient();
         try {
@@ -75,6 +88,91 @@ class PagosController extends AbstractController{
         } catch (\Exception $e) {
             return new JsonResponse(['error' => $e->getMessage()], 500);
         }
+    }
+
+
+
+    #[Route('/create_preference', name: 'create_preference', methods: ['POST'])]
+    public function createPreference(Request $request,ManagerRegistry $doctrine, UsuarioRepository $usuarioRepository): Response
+    {
+        self::authenticate();
+
+        $entityManager = $doctrine->getManager();
+        $jwtPayload = $request->attributes->get('jwt_payload');
+        $usuario = $usuarioRepository->findOneByEmail($jwtPayload->token_info->email);
+
+
+        $data = json_decode($request->getContent(), true);
+
+        // Fill the data about the product(s) being pruchased
+        $product1 = array(
+            "id" => "1234567890",
+            "title" => "Product 1 Title",
+            "description" => "Product 1 Description",
+            "currency_id" => "ARS",
+            "quantity" => 12,
+            "unit_price" => 9.90
+        );
+
+
+
+        // Mount the array of products that will integrate the purchase amount
+        $items = array($product1);
+
+        // Retrieve information about the user (use your own function)
+
+
+        $payer = array(
+            "name" => $usuario->getNombre(),
+            "surname" => $usuario->getNombre(),
+            "email" => $usuario->getEmail(),
+        );
+
+        // Create the request object to be sent to the API when the preference is created
+        $request = self::createPreferenceRequest($items, $payer);
+
+        // Instantiate a new Preference Client
+        $client = new PreferenceClient();
+
+        try {
+            // Send the request that will create the new preference for user's checkout flow
+            $preference = $client->create($request);
+
+            // Useful props you could use from this object is 'init_point' (URL to Checkout Pro) or the 'id'
+            return new JsonResponse($preference);
+        } catch (MPApiException $error) {
+            // Here you might return whatever your app needs.
+            // We are returning null here as an example.
+            return null;
+        }
+    }
+
+
+    function createPreferenceRequest($items, $payer): array
+    {
+        $paymentMethods = [
+            "excluded_payment_methods" => [],
+            "installments" => 1,
+            "default_installments" => 1
+        ];
+
+        $backUrls = array(
+            'success' => 'https://myhouseai.com.ar/',
+            'failure' => 'https://myhouseai.com.ar/'
+        );
+
+        $request = [
+            "items" => $items,
+            "payer" => $payer,
+            "payment_methods" => $paymentMethods,
+            "back_urls" => $backUrls,
+            "statement_descriptor" => "NAME_DISPLAYED_IN_USER_BILLING",
+            "external_reference" => "1234567890",
+            "expires" => false,
+            "auto_return" => 'approved',
+        ];
+
+        return $request;
     }
     
 }
