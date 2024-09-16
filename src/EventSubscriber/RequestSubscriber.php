f8131c3d343019f15074f4da9efdaad30bff79df
seba <sebaporto@gmail.com>
2024-09-16 16:14:53 -0300

    se libera status

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -29,7 +29,7 @@ class RequestSubscriber implements EventSubscriberInterface
         $request->attributes->set('request_start_time', microtime(true));
 
         // Proteger solo ciertas rutas
-        $protectedRoutes = ['generar','historial','process_payment','perfil','payment_controller','create_preference','status','send_emails'];
+        $protectedRoutes = ['generar','historial','process_payment','perfil','payment_controller','create_preference','send_emails'];
         if (!in_array($routeName, $protectedRoutes)) {
             error_log("La request no esta protegida: " .  $routeName);
             return;
